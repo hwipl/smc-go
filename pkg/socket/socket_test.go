03de59014087b02a96d02f4fc1f96b5aad9eabb2
hwipl <33433250+hwipl@users.noreply.github.com>
2020-04-23 18:32:03 +0200

    socket: add ipv6 dial test
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- pkg/socket/socket_test.go
+++ pkg/socket/socket_test.go
@@ -170,4 +170,30 @@ func TestDial(t *testing.T) {
 	cc.Close()
 	cs.Close()
 	l.Close()
+
+	// test ipv6
+	l, err = Listen("::1", 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	port, err = strconv.Atoi(strings.Split(l.Addr().String(), "]:")[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	cc, err = Dial("::1", port)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cs, err = l.Accept()
+	if err != nil {
+		log.Fatal(err)
+	}
+	want = fmt.Sprintf("[::1]:%d", port)
+	got = cc.RemoteAddr().String()
+	if got != want {
+		t.Errorf("RemoteAddr() = %s; want %s", got, want)
+	}
+	cc.Close()
+	cs.Close()
+	l.Close()
 }
