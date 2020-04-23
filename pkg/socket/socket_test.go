1f63b5c3907b8b6ef42b3dc6999e7035e81405a7
hwipl <33433250+hwipl@users.noreply.github.com>
2020-04-23 17:02:52 +0200

    socket: add ipv4 listen tests
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- pkg/socket/socket_test.go
+++ pkg/socket/socket_test.go
@@ -2,13 +2,60 @@ package socket
 
 import (
 	"log"
+	"net"
 	"testing"
 )
 
 func TestListen(t *testing.T) {
-	l, err := Listen("127.0.0.1", 0)
+	var want, got string
+	var l net.Listener
+	var err error
+
+	// test specific ip, specific port, ipv4
+	l, err = Listen("127.0.0.1", 50000)
 	if err != nil {
 		log.Fatal(err)
+	}
+	want = "127.0.0.1:50000"
+	got = l.Addr().String()
+	if got != want {
+		t.Errorf("Addr() = %s; want %s", got, want)
+	}
+	l.Close()
+
+	// test specific ip, random port, ipv4
+	l, err = Listen("127.0.0.1", 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	want = "127.0.0.1:"          // ignore port
+	got = l.Addr().String()[:10] // ignore port
+	if got != want {
+		t.Errorf("Addr() = %s; want %s", got, want)
+	}
+	l.Close()
+
+	// test all ips, specific port, ipv4
+	l, err = Listen("0.0.0.0", 50000)
+	if err != nil {
+		log.Fatal(err)
+	}
+	want = "0.0.0.0:50000"
+	got = l.Addr().String()
+	if got != want {
+		t.Errorf("Addr() = %s; want %s", got, want)
+	}
+	l.Close()
+
+	// test all ips, random port, ipv4
+	l, err = Listen("0.0.0.0", 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	want = "0.0.0.0:"           // ignore port
+	got = l.Addr().String()[:8] // ignore port
+	if got != want {
+		t.Errorf("Addr() = %s; want %s", got, want)
 	}
 	l.Close()
 }
