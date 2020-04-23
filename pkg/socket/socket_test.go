cc414e70531f9489564afe85560bfa85d7629d38
hwipl <33433250+hwipl@users.noreply.github.com>
2020-04-23 18:12:16 +0200

    socket: move specific listen port tests into separate functions
    Specific port tests might fail, because address/port is already in use.
    Move them into separate functions and just skip these test in case of
    listen error.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- pkg/socket/socket_test.go
+++ pkg/socket/socket_test.go
@@ -6,22 +6,85 @@ import (
 	"testing"
 )
 
-func TestListen(t *testing.T) {
+// Specific port tests could fail because address/port is already in use.
+// Run them in individual test functions and skip in case of listen error.
+
+func TestListenSpecificIPv4(t *testing.T) {
 	var want, got string
 	var l net.Listener
 	var err error
 
 	// test specific ip, specific port, ipv4
-	l, err = Listen("127.0.0.1", 50000)
+	l, err = Listen("127.0.0.1", 50001)
 	if err != nil {
-		log.Fatal(err)
+		t.Skip(err)
 	}
-	want = "127.0.0.1:50000"
+	want = "127.0.0.1:50001"
 	got = l.Addr().String()
 	if got != want {
 		t.Errorf("Addr() = %s; want %s", got, want)
 	}
 	l.Close()
+}
+
+func TestListenSpecificAllIPv4(t *testing.T) {
+	var want, got string
+	var l net.Listener
+	var err error
+
+	// test all ips, specific port, ipv4
+	l, err = Listen("0.0.0.0", 50002)
+	if err != nil {
+		t.Skip(err)
+	}
+	want = "0.0.0.0:50002"
+	got = l.Addr().String()
+	if got != want {
+		t.Errorf("Addr() = %s; want %s", got, want)
+	}
+	l.Close()
+}
+
+func TestListenSpecificIPv6(t *testing.T) {
+	var want, got string
+	var l net.Listener
+	var err error
+
+	// test specific ip, specific port, ipv6
+	l, err = Listen("::1", 50003)
+	if err != nil {
+		t.Skip(err)
+	}
+	want = "[::1]:50003"
+	got = l.Addr().String()
+	if got != want {
+		t.Errorf("Addr() = %s; want %s", got, want)
+	}
+	l.Close()
+}
+
+func TestListenSpecificAllIPv6(t *testing.T) {
+	var want, got string
+	var l net.Listener
+	var err error
+
+	// test all ips, specific port, ipv6
+	l, err = Listen("::", 50004)
+	if err != nil {
+		t.Skip(err)
+	}
+	want = "[::]:50004"
+	got = l.Addr().String()
+	if got != want {
+		t.Errorf("Addr() = %s; want %s", got, want)
+	}
+	l.Close()
+}
+
+func TestListen(t *testing.T) {
+	var want, got string
+	var l net.Listener
+	var err error
 
 	// test specific ip, random port, ipv4
 	l, err = Listen("127.0.0.1", 0)
@@ -30,18 +93,6 @@ func TestListen(t *testing.T) {
 	}
 	want = "127.0.0.1:"          // ignore port
 	got = l.Addr().String()[:10] // ignore port
-	if got != want {
-		t.Errorf("Addr() = %s; want %s", got, want)
-	}
-	l.Close()
-
-	// test all ips, specific port, ipv4
-	l, err = Listen("0.0.0.0", 50000)
-	if err != nil {
-		log.Fatal(err)
-	}
-	want = "0.0.0.0:50000"
-	got = l.Addr().String()
 	if got != want {
 		t.Errorf("Addr() = %s; want %s", got, want)
 	}
@@ -59,18 +110,6 @@ func TestListen(t *testing.T) {
 	}
 	l.Close()
 
-	// test specific ip, specific port, ipv6
-	l, err = Listen("::1", 50000)
-	if err != nil {
-		log.Fatal(err)
-	}
-	want = "[::1]:50000"
-	got = l.Addr().String()
-	if got != want {
-		t.Errorf("Addr() = %s; want %s", got, want)
-	}
-	l.Close()
-
 	// test specific ip, random port, ipv6
 	l, err = Listen("::1", 0)
 	if err != nil {
@@ -78,18 +117,6 @@ func TestListen(t *testing.T) {
 	}
 	want = "[::1]:"             // ignore port
 	got = l.Addr().String()[:6] // ignore port
-	if got != want {
-		t.Errorf("Addr() = %s; want %s", got, want)
-	}
-	l.Close()
-
-	// test all ips, specific port, ipv6
-	l, err = Listen("::", 50000)
-	if err != nil {
-		log.Fatal(err)
-	}
-	want = "[::]:50000"
-	got = l.Addr().String()
 	if got != want {
 		t.Errorf("Addr() = %s; want %s", got, want)
 	}
