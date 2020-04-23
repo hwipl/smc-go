14acfe18f67e818ac111642984eba7f536f55d9a
hwipl <33433250+hwipl@users.noreply.github.com>
2020-04-23 18:18:36 +0200

    socket: add ipv4 dial test
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- pkg/socket/socket_test.go
+++ pkg/socket/socket_test.go
@@ -1,8 +1,11 @@
 package socket
 
 import (
+	"fmt"
 	"log"
 	"net"
+	"strconv"
+	"strings"
 	"testing"
 )
 
@@ -132,5 +135,39 @@ func TestListen(t *testing.T) {
 	if got != want {
 		t.Errorf("Addr() = %s; want %s", got, want)
 	}
+	l.Close()
+}
+
+func TestDial(t *testing.T) {
+	var want, got string
+	var cc, cs net.Conn
+	var l net.Listener
+	var err error
+	var port int
+
+	// test ipv4
+	l, err = Listen("127.0.0.1", 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	port, err = strconv.Atoi(strings.Split(l.Addr().String(), ":")[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	cc, err = Dial("127.0.0.1", port)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cs, err = l.Accept()
+	if err != nil {
+		log.Fatal(err)
+	}
+	want = fmt.Sprintf("127.0.0.1:%d", port)
+	got = cc.RemoteAddr().String()
+	if got != want {
+		t.Errorf("RemoteAddr() = %s; want %s", got, want)
+	}
+	cc.Close()
+	cs.Close()
 	l.Close()
 }
