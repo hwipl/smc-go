57240457eafc4fee347960bdce2301849f69d0b0
hwipl <33433250+hwipl@users.noreply.github.com>
2020-04-26 15:34:38 +0200

    socket: add no host dial test
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- pkg/socket/dial_test.go
+++ pkg/socket/dial_test.go
@@ -64,6 +64,35 @@ func TestDialIPv6Zone(t *testing.T) {
 	l.Close()
 }
 
+func TestDialNoHost(t *testing.T) {
+	var want, got string
+	var cc, cs net.Conn
+	var l net.Listener
+	var err error
+
+	// test no host
+	l, err = Listen("127.0.0.1:50103")
+	if err != nil {
+		t.Skip(err)
+	}
+	cc, err = Dial(":50103")
+	if err != nil {
+		log.Fatal(err)
+	}
+	cs, err = l.Accept()
+	if err != nil {
+		log.Fatal(err)
+	}
+	want = l.Addr().String()
+	got = cc.RemoteAddr().String()
+	if got != want {
+		t.Errorf("RemoteAddr() = %s; want %s", got, want)
+	}
+	cc.Close()
+	cs.Close()
+	l.Close()
+}
+
 func TestDial(t *testing.T) {
 	var want, got string
 	var cc, cs net.Conn
