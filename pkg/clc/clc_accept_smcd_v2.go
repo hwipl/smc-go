00931e5f3c89755bbaaf2aa04d586ff2fe63294f
hwipl <33433250+hwipl@users.noreply.github.com>
2020-10-29 15:47:40 +0100

    clc: add pkg/clc/clc_accept_smcd_v2.go
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ pkg/clc/clc_accept_smcd_v2.go
@@ -0,0 +1,33 @@
+package clc
+
+const (
+	// AcceptSMCDv2Len is the miminum length of a SMCv2 SMC-D Accept msg
+	AcceptSMCDv2Len = 78
+	// AcceptSMCDv2FCELen is the length of a SMCv2 SMC-D Accept with FCE
+	AcceptSMCDv2FCELen = 114
+)
+
+// AcceptSMCDv2 stores a SMCv2 CLC SMC-D Accept message
+type AcceptSMCDv2 struct {
+	Raw
+	Header
+	GID        uint64   // Sender GID
+	Token      uint64   // DMB token
+	DMBEIdx    uint8    // DMBE index
+	DMBESize   RMBESize // 4 bits buf size (compressed)
+	reserved   byte     // 4 bits reserved
+	reserved2  [2]byte
+	LinkID     uint32 // Link identifier
+	ISMv2VCHID uint16 // ISMv2 VCHID
+	EID        EID    // EID
+	reserved3  [8]byte
+
+	// First Contact Extension; only present if first contact flag set
+	reserved4 byte
+	OSType    OSType // 4 bits
+	Release   uint8  // 4 bits
+	reserved5 [2]byte
+	Hostname  EID // hostname has same format as EID
+
+	Trailer
+}
