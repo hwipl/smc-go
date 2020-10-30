b25cbf70141e27b84587e80d7654723653f8a706
hwipl <33433250+hwipl@users.noreply.github.com>
2020-10-30 12:38:15 +0100

    clc: add method Parse() to accept smc-d v2
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- pkg/clc/clc_accept_smcd_v2.go
+++ pkg/clc/clc_accept_smcd_v2.go
@@ -1,7 +1,9 @@
 package clc
 
 import (
+	"encoding/binary"
 	"fmt"
+	"log"
 )
 
 const (
@@ -86,4 +88,89 @@ func (ac *AcceptSMCDv2) Reserved() string {
 		ac.Token, ac.DMBEIdx, ac.DMBESize, ac.reserved,
 		ac.reserved2, ac.LinkID, ac.ISMv2VCHID, &ac.EID,
 		ac.reserved3, ac.fceReserved(), ac.Trailer)
+}
+
+// Parse parses the SMCv2 CLC SMC-D Accept message in buf
+func (ac *AcceptSMCDv2) Parse(buf []byte) {
+	// save raw message bytes
+	ac.Raw.Parse(buf)
+
+	// parse clc header
+	ac.Header.Parse(buf)
+
+	// check if message is long enough
+	if ac.Length < AcceptSMCDv2Len || len(buf) < AcceptSMCDv2Len {
+		err := "Error parsing SMC-Dv2 CLC Accept: message too short"
+		if ac.Type == TypeConfirm {
+			err = "Error parsing SMC-Dv2 CLC Confirm: " +
+				"message too short"
+		}
+		log.Println(err)
+		errDump(buf[:ac.Length])
+		return
+	}
+
+	// skip clc header
+	buf = buf[HeaderLen:]
+
+	// smcd GID
+	ac.GID = binary.BigEndian.Uint64(buf[:8])
+	buf = buf[8:]
+
+	// smcd Token
+	ac.Token = binary.BigEndian.Uint64(buf[:8])
+	buf = buf[8:]
+
+	// dmbe index
+	ac.DMBEIdx = uint8(buf[0])
+	buf = buf[1:]
+
+	// 1 byte bitfield: dmbe size (4 bits), reserved (4 bits)
+	ac.DMBESize = RMBESize((uint8(buf[0]) & 0b11110000) >> 4)
+	ac.reserved = buf[0] & 0b00001111
+	buf = buf[1:]
+
+	// reserved
+	copy(ac.reserved2[:], buf[:2])
+	buf = buf[2:]
+
+	// link id
+	ac.LinkID = binary.BigEndian.Uint32(buf[:4])
+	buf = buf[4:]
+
+	// ISMv2 VCHID
+	ac.ISMv2VCHID = binary.BigEndian.Uint16(buf[:2])
+	buf = buf[2:]
+
+	// EID
+	copy(ac.EID[:], buf[:EIDLen])
+	buf = buf[EIDLen:]
+
+	// reserved
+	copy(ac.reserved3[:], buf[:8])
+	buf = buf[8:]
+
+	// parse First Contact Extension (FCE) if present
+	if ac.Length == AcceptSMCDv2FCELen {
+		// reserved
+		ac.reserved4 = buf[0]
+		buf = buf[1:]
+
+		// OS type (4 bits)
+		ac.OSType = OSType(buf[0] >> 4)
+		// Release (4 bits)
+		ac.Release = buf[0] & 0b00001111
+		buf = buf[1:]
+
+		// reserved
+		copy(ac.reserved5[:], buf[:2])
+		buf = buf[2:]
+
+		// hostname
+		copy(ac.Hostname[:], buf[:EIDLen])
+		buf = buf[EIDLen:]
+	}
+
+	// save trailer
+	ac.Trailer.Parse(buf)
 }
