cbd6ead4565d6080a0479e1e30d978c22d9272b3
hwipl <33433250+hwipl@users.noreply.github.com>
2020-10-30 12:43:37 +0100

    clc: add test for smc-d v2 accept without fce
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- pkg/clc/clc_accept_smcd_v2_test.go
+++ pkg/clc/clc_accept_smcd_v2_test.go
@@ -6,6 +6,57 @@ import (
 	"testing"
 )
 
+func TestParseSMCDv2Accept(t *testing.T) {
+	// prepare message
+	msgBytes := "e2d4c3c4" + "02" + "004e" + "2" + "1" + "0123456789abcdef" +
+		"0123456789abcdef" + "ff" + "1" + "0" + "0000" + "ffffffff" +
+		"0123" +
+		"546869734973534d4376324549443031" +
+		"00000000000000000000000000000000" +
+		"0000000000000000" +
+		"e2d4c3c4"
+	msg, err := hex.DecodeString(msgBytes)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// parse message
+	clc, clcLen := NewMessage(msg)
+	clc.Parse(msg)
+
+	// check message length
+	if clcLen != AcceptSMCDv2Len {
+		t.Errorf("clcLen = %d; want %d", clcLen, AcceptSMCDv2Len)
+	}
+
+	// check output message without reserved fields
+	want := "Accept: Eyecatcher: SMC-D, Type: 2 (Accept), Length: 78, " +
+		"Version: 2, First Contact: 0, Path: SMC-D, " +
+		"SMC-D GID: 81985529216486895, " +
+		"SMC-D Token: 81985529216486895, DMBE Index: 255, " +
+		"DMBE Size: 1 (32768), Link ID: 4294967295, " +
+		"ISMv2 VCHID: 291, EID: ThisIsSMCv2EID01, " +
+		"Trailer: SMC-D"
+	got := clc.String()
+	if got != want {
+		t.Errorf("clc.String() = %s; want %s", got, want)
+	}
+
+	// check output message with reserved fields
+	want = "Accept: Eyecatcher: SMC-D, Type: 2 (Accept), " +
+		"Length: 78, Version: 2, First Contact: 0, Reserved: 0x0, " +
+		"Path: SMC-D, SMC-D GID: 81985529216486895, " +
+		"SMC-D Token: 81985529216486895, " +
+		"DMBE Index: 255, DMBE Size: 1 (32768), Reserved: 0x0, " +
+		"Reserved: 0x0000, Link ID: 4294967295, " +
+		"ISMv2 VCHID: 291, EID: ThisIsSMCv2EID01, " +
+		"Reserved: 0x0000000000000000, Trailer: SMC-D"
+	got = clc.Reserved()
+	if got != want {
+		t.Errorf("clc.Reserved() = %s; want %s", got, want)
+	}
+}
+
 func TestParseSMCDv2FCEAccept(t *testing.T) {
 	// prepare message
 	msgBytes := "e2d4c3c4" + "02" + "0072" + "2" + "9" + "0123456789abcdef" +
