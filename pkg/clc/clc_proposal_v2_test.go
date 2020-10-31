8fa9d128450d16223df983ce35a2c40be419326d
hwipl <33433250+hwipl@users.noreply.github.com>
2020-10-31 14:28:41 +0100

    clc: add test for smc v2 proposal with no ip prefixes
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- pkg/clc/clc_proposal_v2_test.go
+++ pkg/clc/clc_proposal_v2_test.go
@@ -6,6 +6,98 @@ import (
 	"testing"
 )
 
+func TestParseCLCProposalV2SMCB(t *testing.T) {
+	// prepare smc-b (r + d) proposal v2 message:
+	// Eyecatcher, Type, Length, Version, Pathv2+Path, SenderPeerID
+	msgBytes := "e2d4c3d9" + "01" + "00d6" + "2" + "e" +
+		"394498039babcdef" +
+		// IBGID
+		"fe800000000000009a039bfffeabcdef" +
+		// IBMAC, IPAreaOffset, SMCDGID
+		"98039babcdef" + "0028" + "0123456789abcdef" +
+		// ISMv2VCHID, SMCv2Offset, reserved
+		"1234" + "0000" + "000000000000000000000000" +
+		// reserved
+		"00000000000000000000000000000000" +
+		// EIDNumber, GIDNumber, reserved3,
+		// Release+reserved4+SEIDInd, reserved5, SMCDv2Off
+		"01" + "01" + "00" + "01" + "0000" + "00" +
+		// SMCDv2Off 1 byte, reserved6 15 bytes
+		"40" + "000000000000000000000000000000" +
+		// reserved6 16 bytes
+		"00000000000000000000000000000000" +
+		// reserved6 1 byte, EID 15 bytes
+		"00" + "546869734973534d43763245494430" +
+		// EID 16 bytes
+		"31000000000000000000000000000000" +
+		// EID 1 byte, SEID 15 bytes
+		"00" + "546869734973534d43763245494430" +
+		// SEID 16 bytes
+		"32000000000000000000000000000000" +
+		// SEID 1 byte, reserved7 15 bytes
+		"00" + "000000000000000000000000000000" +
+		// reserved7 1 byte, GID 8 bytes, VCHID 2 bytes
+		"00" + "abcdef0123456789" + "0123" +
+		// Trailer
+		"e2d4c3d9"
+	msg, err := hex.DecodeString(msgBytes)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// parse message
+	proposal, proposalLen := NewMessage(msg)
+	proposal.Parse(msg)
+
+	// check message length
+	if proposalLen != 214 {
+		t.Errorf("proposalLen = %d; want %d", proposalLen, 214)
+	}
+
+	// check output message without reserved fields
+	want := "Proposal: Eyecatcher: SMC-R, Type: 1 (Proposal), " +
+		"Length: 214, Version: 2, Pathv2: SMC-R + SMC-D, " +
+		"Path: No SMC-R/SMC-D, Peer ID: 14660@98:03:9b:ab:cd:ef, " +
+		"SMC-R GID: fe80::9a03:9bff:feab:cdef, " +
+		"RoCE MAC: 98:03:9b:ab:cd:ef, IP Area Offset: 40, " +
+		"SMC-D GID: 81985529216486895, ISMv2 VCHID: 4660, " +
+		"SMCv2 Extension Offset: 0, " +
+		"EID Number: 1, GID Number: 1, Release: 0, " +
+		"SEID Indicator: 1, SMC-Dv2 Extension Offset: 64, " +
+		"EID Area: [EID 0: ThisIsSMCv2EID01], " +
+		"SEID: ThisIsSMCv2EID02, " +
+		"GID Area: [GID 0: 12379813738877118345, VCHID 0: 291], " +
+		"Trailer: SMC-R"
+	got := proposal.String()
+	if got != want {
+		t.Errorf("proposal.String() = %s; want %s", got, want)
+	}
+
+	// check output message with reserved fields
+	want = "Proposal: Eyecatcher: SMC-R, Type: 1 (Proposal), " +
+		"Length: 214, Version: 2, Pathv2: SMC-R + SMC-D, " +
+		"Path: No SMC-R/SMC-D, Peer ID: 14660@98:03:9b:ab:cd:ef, " +
+		"SMC-R GID: fe80::9a03:9bff:feab:cdef, " +
+		"RoCE MAC: 98:03:9b:ab:cd:ef, IP Area Offset: 40, " +
+		"SMC-D GID: 81985529216486895, ISMv2 VCHID: 4660, " +
+		"SMCv2 Extension Offset: 0, " +
+		"Reserved: 0x00000000000000000000000000000000000000000000" +
+		"000000000000, " +
+		"EID Number: 1, GID Number: 1, Reserved: 0x0, Release: 0, " +
+		"Reserved: 0x0, SEID Indicator: 1, Reserved: 0x0000, " +
+		"SMC-Dv2 Extension Offset: 64, Reserved: 0x0000000000000000" +
+		"000000000000000000000000000000000000000000000000, " +
+		"EID Area: [EID 0: ThisIsSMCv2EID01], " +
+		"SEID: ThisIsSMCv2EID02, " +
+		"Reserved: 0x00000000000000000000000000000000, " +
+		"GID Area: [GID 0: 12379813738877118345, VCHID 0: 291], " +
+		"Trailer: SMC-R"
+	got = proposal.Reserved()
+	if got != want {
+		t.Errorf("proposal.Reserved() = %s; want %s", got, want)
+	}
+}
+
 func TestParseCLCProposalV2SMCBIPv4(t *testing.T) {
 	// prepare smc-b (r + d) ipv4 proposal v2 message:
 	// Eyecatcher, Type, Length, Version, Pathv2+Path, SenderPeerID
